2998c607d88e00f477c2f6208e2348ebfcadd07b
ho757uff <robindebessel.pro@gmail.com>
2024-04-24 23:16:02 +0200

    Working Pop-up Extension

--- chrono-dao-assets-tracker/popup.tsx
+++ chrono-dao-assets-tracker/popup.tsx
@@ -1,31 +1,33 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 function IndexPopup() {
-  const [currentUrl, setCurrentUrl] = useState<string>(initialState: "")
-  
-  const getCurrentUrl = () => {
-    const [tab] = await chrome.tabs.query(queryInfo: { active: true, currentWindow: true })
-    setCurrentUrl(value: tab.url)
-  }
+  const [currentUrl, setCurrentUrl] = useState<string>(""); // Suppression de l'argument initialState
 
-useEffect(effect: () : void => {
-  getCurrentUrl()
-}, deps: [currentUrl])
+  useEffect(() => { // Correction de l'utilisation de useEffect
+    getCurrentUrl();
+  }, []); // Suppression du tableau de dépendances, car nous n'avons pas besoin de re-exécuter useEffect en fonction d'une dépendance
+
+  const getCurrentUrl = async () => { // Ajout de async pour permettre l'utilisation de await
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true }); // Correction de la syntaxe
+    setCurrentUrl(tab.url); // Correction de la syntaxe
+  };
 
   return (
     <div
       style={{
         padding: 16
-      }}>
+      }}
+    >
       <h1>
         You're currently at {currentUrl}
       </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
+      {/* L'élément input nécessite une fonction onChange pour mettre à jour la valeur */}
+      <input onChange={(e) => setCurrentUrl(e.target.value)} value={currentUrl} /> 
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
       </a>
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
