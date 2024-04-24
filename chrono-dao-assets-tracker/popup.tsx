077175b12bb395ac5b687f207b527af7342f16ed
ho757uff <robindebessel.pro@gmail.com>
2024-04-24 20:23:42 +0200

    inB4 : code update proposal

--- chrono-dao-assets-tracker/popup.tsx
+++ chrono-dao-assets-tracker/popup.tsx
@@ -1,20 +1,25 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [currentUrl, setCurrentUrl] = useState<string>(initialState: "")
+  
+  const getCurrentUrl = () => {
+    const [tab] = await chrome.tabs.query(queryInfo: { active: true, currentWindow: true })
+    setCurrentUrl(value: tab.url)
+  }
+
+useEffect(effect: () : void => {
+  getCurrentUrl()
+}, deps: [currentUrl])
 
   return (
     <div
       style={{
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
+      <h1>
+        You're currently at {currentUrl}
+      </h1>
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
