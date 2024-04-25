417a3534e2488da5d0f8b0f9dacea5d474b84fd2
ho757uff <robindebessel.pro@gmail.com>
2024-04-25 16:29:02 +0200

    Working new full page tab

--- chronodao-assets-tracker/popup2.tsx
+++ chronodao-assets-tracker/popup2.tsx
@@ -1,11 +1,9 @@
-
-
 import React, { useEffect } from 'react';
 
 const Popup: React.FC = () => {
   useEffect(() => {
     // Ouvrir un nouvel onglet avec une page vide contenant "Hello World"
-    chrome.tabs.create({ url: 'data:text/html,<h1>Hello World</h1>' });
+    chrome.tabs.create({ url: 'chrome://newtab' });
   }, []);
 
   return null; // Pas besoin de rendre quoi que ce soit dans la page contextuelle (popup)
